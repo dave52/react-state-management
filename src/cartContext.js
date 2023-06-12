b015b7ab6b8954ad52c09c2305dc490e4d881c01
Dave Hudson <davehudson52@gmail.com>
2023-06-12 09:09:51 -0500

    chore: proj clean up files

--- src/cartContext.js
+++ src/cartContext.js
@@ -5,30 +5,34 @@ import cartReducer from "./cartReducer";
 const CartContext = React.createContext(null);
 
 let initialCart;
-  try {
-    initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
-  } catch {
-    console.error("The cart could not be parsed into JSON.");
-    initialCart = [];
-  }
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
 
 export function CartProvider(props) {
   const [cart, dispatch] = useReducer(cartReducer, initialCart);
-  useEffect(() => localStorage.setItem("cart"), JSON.stringify(cart), [cart]);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
   const contextValue = {
     cart,
-    dispatch
+    dispatch,
   };
 
   return (
-    <CartContext.Provider value={contextValue}>{props.children}</CartContext.Provider>
-  )
+    <CartContext.Provider value={contextValue}>
+      {props.children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCart() {
   const context = useContext(CartContext);
   if (!context) {
-    throw new Error("useCart must be used within a CartProvider. Wrap a parent component in <CartProvider> to fix this error.")
+    throw new Error(
+      "useCart must be used within a CartProvider. Wrap a parent component in <CartProvider> to fix this error."
+    );
   }
   return context;
 }
